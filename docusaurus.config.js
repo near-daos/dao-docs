091f0837bc0cc08ebe3878495d106031417fb4be
Edward Yu <edwardkcyu@gmail.com>
2022-10-13 14:33:33 +1300

    fix baseUrl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'AstroDAO',
   tagline: 'AstroDAO',
   url: 'https://astro-dao.com',
-  baseUrl: '/dao-docs',
+  baseUrl: '/dao-docs/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
